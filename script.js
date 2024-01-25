f096571e3694a8092b8fa54f991bd2c9f67e3f08
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2024-01-25 15:16:18 -0500

    Update form inputs in modal and refactor code structure

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// grab add button by id
-// add event listener to it
-// on event mousedown
-// prompt user for Name, Author, and Read Boolean
+// DOM elements
+const library = document.querySelector('.library');
+const add = document.querySelector('#add');
+const modal = document.querySelector('#modal');
+const modalBtn = document.querySelector('.modal-add');
+const modalTitle = document.querySelector('.modal-title');
+const modalAuthor = document.querySelector('.modal-author');
+const modalStatus = document.querySelector('.modal-status');
 
-const library = document.querySelector(".library");
-const add = document.querySelector("#add");
-
-const modal = document.querySelector("#modal");
-const modalBtn = document.querySelector(".modal-add");
-const modalTitle = document.querySelector(".modal-title");
-const modalAuthor = document.querySelector(".modal-author");
-const modalStatus = document.querySelector(".modal-status");
-
+// Book class
 class Book {
   constructor(title, author, status) {
     this.title = title;
@@ -20,39 +16,42 @@ class Book {
   }
 }
 
+// Function to clear modal inputs
 function modalClear() {
-  modalTitle.value = "";
-  modalAuthor.value = "";
+  modalTitle.value = '';
+  modalAuthor.value = '';
   modalStatus.checked = false;
 }
 
+// Construct card element for a book and append it to the library
 const constrCard = function (book) {
-  const index = document.querySelectorAll(".card").length + 1;
+  const index = document.querySelectorAll('.card').length + 1;
 
-  const newCard = document.createElement("div");
-  newCard.classList.add("card");
-  newCard.setAttribute("id", `card${index}`);
+  const newCard = document.createElement('div');
+  newCard.classList.add('card');
+  newCard.setAttribute('id', `card${index}`);
 
-  const newTitle = document.createElement("h2");
+  const newTitle = document.createElement('h2');
   newTitle.textContent = book.title;
 
-  const newAuthor = document.createElement("p");
-  newAuthor.classList.add("author");
+  const newAuthor = document.createElement('p');
+  newAuthor.classList.add('author');
   newAuthor.textContent = book.author;
 
   if (!book.status) {
-    newCard.classList.add("unread");
+    newCard.classList.add('unread');
   }
 
   library.appendChild(newCard);
   newCard.appendChild(newTitle).appendChild(newAuthor);
 };
 
-add.addEventListener("mouseup", () => {
+// Event listeners
+add.addEventListener('mouseup', () => {
   modal.showModal();
 });
 
-modalBtn.addEventListener("mouseup", () => {
+modalBtn.addEventListener('mouseup', () => {
   const title = modalTitle.value;
   const author = modalAuthor.value;
   const status = modalStatus.checked;
